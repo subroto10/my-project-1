8dee5f0ffc3ede6af38ca0ab4d8bb03a6f1a9659
Subroto Saha <119009682+subroto10@users.noreply.github.com>
2022-12-23 21:30:00 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,12 @@
+
+$(function(){
+   feather.replace();
+   $('.popular_slider').slick({
+    slidesToShow:3,
+    arrows:false,
+    centerMode: true,
+    centerPadding:0,
+    focusOnSelect: true,
+
+   });
+})
